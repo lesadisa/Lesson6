97b5fac2bed81d298ccfcbe9ce85fe1a3081e6f0
lesadisa2 <55075793+lesadisa2@users.noreply.github.com>
2021-09-29 14:26:03 +0300

    startDzLesson_Umnogenie_Podchet

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -51,10 +51,10 @@ public class Main {
 
 
         assert list != null;
-  //      list.forEach(System.out::println);
-
+        //      list.forEach(System.out::println);
 
         //таблица умножения вариант 1
+        System.out.println("Таблица умножения вариант 1");
         for (int i = 1; i <= 10; i++) {
 
             for (int y = 1; y <= 10; y++) {
@@ -64,49 +64,26 @@ public class Main {
         }
 
         //таблица умножения вариант 2
+        System.out.println("Таблица умножения вариант 2");
         IntStream.rangeClosed(1, 10).forEach(i -> {
             IntStream.rangeClosed(1, 10)
                     .forEach(j -> System.out.print(i * j + " "));
             System.out.println();
         });
+
+        // поиск количеств вхождения
+        System.out.println("Поиск количеств вхождения без класса Scanner");
         String strsearch = "страда";
-/*       // String strnext = String.valueOf(list) + "";
-
-
-        if (0 == 1) {
-            System.out.println("ok");
-        } else {
-            //System.out.println(strLowecase.contains(strsearch));
-            System.out.println(strsearch.regionMatches(1, strnext, 1, 3));
+        String[] strtext = String.valueOf(list).split("\\s");
+        int z = 0;
+        for (String s : strtext) {
+            if (s.contains(strsearch)) {
+                System.out.println(s.contains(strsearch) + " " + s);
+                z = z + 1;
+            }
         }
-
-*/
-        /*
-
-
-Метод принимает следующие параметры:
-
-ignoreCase: надо ли игнорировать регистр символов при сравнении. Если значение true, регистр игнорируется
-
-toffset: начальный индекс в вызывающей строке, с которого начнется сравнение
-
-other: строка, с которой сравнивается вызывающая
-
-oofset: начальный индекс в сравниваемой строке, с которого начнется сравнение
-
-len: количество сравниваемых символов в обеих строках
-
-  System.out.println(strLowecase.getChars(1,5,strLowecase2,strnext))
-        Если надо извлечь сразу группу символов или подстроку, то можно использовать метод getChars(int srcBegin, int srcEnd, char[] dst, int dstBegin). Он принимает следующие параметры:
-
-srcBegin: индекс в строке, с которого начинается извлечение символов
-
-srcEnd: индекс в строке, до которого идет извлечение символов
-
-dst: массив символов, в который будут извлекаться символы
-
-dstBegin: индекс в массиве dst, с которого надо добавлять извлеченные из строки символы
-         */
+        System.out.println(z);
     }
-
 }
+
+
